13e8a669ff3090b3885cfd8837d838328ba0e5c7
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-01-22 15:09:28 +0000

    commit

---
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class StatefulGreetingWithCallback extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        }, () => { /* call back function */
+            console.log('new state', this.state.introduction);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreetingWithCallback;
