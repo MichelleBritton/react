e65ea1b4db601950357e35e102e3ea1c211f9456
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-01-22 16:27:20 +0000

    commit

---
+++ my-app/src/components/Es7Snippets.js
@@ -0,0 +1,80 @@
+/* To create a functional component, type rfce
+
+import React from 'react'
+
+function Es7Snippets() {
+  return (
+    <div>Es7Snippets</div>
+  )
+}
+
+export default Es7Snippets
+
+*/
+
+
+
+/* To create a functional component as above but as an arrow function, type rafce 
+
+import React from 'react'
+
+const Es7Snippets = () => {
+  return (
+    <div>Es7Snippets</div>
+  )
+}
+
+export default Es7Snippets
+
+*/
+
+
+
+/* To create a class component, type rce
+
+import React, { Component } from 'react'
+
+export class Es7Snippets extends Component {
+  render() {
+    return (
+      <div>Es7Snippets</div>
+    )
+  }
+}
+
+export default Es7Snippets
+
+*/
+
+
+
+/* To create a constructor, type rconst
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+    
+  }
+}
+
+*/
+
+
+
+/* To log something to the console, type clg
+
+console.log(first)
+
+*/
+
+
+
+/* To log an object along with its name as a string, type clo
+
+console.log('first', first)
+
+*/
+
+
+
