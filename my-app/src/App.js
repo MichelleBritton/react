473bd0a8903949b33a45c643e8b276ff2ca9a097
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-01-29 13:43:01 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
-import EventBinding from './components/EventBinding';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
-import MethodsAsPropsParent from './components/MethodsAsPropsParent';
-import RenderingLists from './components/RenderingLists';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
-import LifeCyclesCDU from './components/LifeCyclesCDU';
+// import FunctionalGreeting from "./components/FunctionalGreeting";
+// import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import EventsClass from './components/EventsClass';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventBinding from './components/EventBinding';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// import NestingComponents from './components/NestingComponents';
+// import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+// import RenderingLists from './components/RenderingLists';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
+import LifeCyclesCWU from './components/LifeCyclesCWU';
 
 function App() {
   return (
@@ -29,11 +30,12 @@ function App() {
       <EventBinding />
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true} />
-      <NestingComponents /> */}
-      {/* <MethodsAsPropsParent /> */}
-      {/* <RenderingLists /> */}
-      {/* <LifeCyclesCDM /> */}
-      <LifeCyclesCDU />
+      <NestingComponents />
+      <MethodsAsPropsParent />
+      <RenderingLists />
+      <LifeCyclesCDM />
+      <LifeCyclesCDU /> */}
+      <LifeCyclesCWU />
     </div>
   );
 }
